cc2a0e838b9d9c55c5ea20af2b785225dd13953f
Fstream7 <ivan.tkachuk@fotbo.org>
2023-09-25 09:52:08 +0300

    set filter for router

--- app/handlers/get_media_id.py
+++ app/handlers/get_media_id.py
@@ -3,18 +3,19 @@ from aiogram.types import Message
 from filters.chat_type import ChatTypeFilter
 
 router = Router()
+router.message.filter(ChatTypeFilter(chat_type=["private"]))
 
 
-@router.message(F.sticker, ChatTypeFilter(chat_type=["private"]))
+@router.message(F.sticker)
 async def message_with_sticker(message: Message):
     await message.answer(f"Sticker file_id {message.sticker.file_id}")
 
 
-@router.message(F.photo, ChatTypeFilter(chat_type=["private"]))
+@router.message(F.photo)
 async def message_with_photo(message: Message):
     await message.answer(f"Photo file_id {message.photo[-1].file_id}")
 
 
-@router.message(F.animation, ChatTypeFilter(chat_type=["private"]))
+@router.message(F.animation)
 async def message_with_animation(message: Message):
     await message.answer(f"Animation file_id {message.animation.file_id}")
