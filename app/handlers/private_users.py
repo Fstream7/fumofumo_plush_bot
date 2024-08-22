b5a0e6a1eaf74c5cb1aa851b0503051b65a1922d
Fstream7 <ivan.tkachuk@fotbo.org>
2024-08-22 19:23:59 +0300

    add user info on forwarding message

--- app/handlers/private_users.py
+++ app/handlers/private_users.py
@@ -16,6 +16,10 @@ router = Router()
     F.content_type.in_({'photo', 'video', 'audio', 'document'}))
 @media_group_handler
 async def media_group(messages: List[types.Message], bot: Bot) -> None:
+    await bot.send_message(
+        Config.ADMIN_CHAT_ID,
+        f"New message from {messages[-1].from_user.full_name} @{messages[-1].from_user.username}({messages[-1].from_user.id})"
+    )
     await bot(ForwardMessages(chat_id=Config.ADMIN_CHAT_ID,
                               from_chat_id=messages[-1].chat.id,
                               message_ids=[message.message_id for message in messages]))
@@ -24,7 +28,11 @@ async def media_group(messages: List[types.Message], bot: Bot) -> None:
 
 
 @router.message(ChatTypeFilter(chat_type=["private"]))
-async def process_propose(message: types.Message) -> None:
+async def process_propose(message: types.Message, bot: Bot) -> None:
+    await bot.send_message(
+        Config.ADMIN_CHAT_ID,
+        f"New message from {message.from_user.full_name} @{message.from_user.username}({message.from_user.id})"
+    )
     await message.forward(Config.ADMIN_CHAT_ID)
     await message.answer(Messages.thanks_message.format(user_full_name=message.from_user.full_name))
     await message.answer_sticker(sticker=choice(Messages.thanks_sticker))
