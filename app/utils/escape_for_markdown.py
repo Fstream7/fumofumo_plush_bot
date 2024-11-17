b475194b9e296ffb3ec4b2075dded3f191561252
Fstream7 <ivan.tkachuk@fotbo.org>
2024-11-17 18:06:19 +0200

    escape markdown symbols in name for db

---
+++ app/utils/escape_for_markdown.py
@@ -0,0 +1,10 @@
+import re
+
+
+def escape_markdown(text: str) -> str:
+    '''
+    https://core.telegram.org/bots/api#formatting-options
+    '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!' must be escaped with the preceding character '\'.
+    '''
+    special_chars = r"[\_\*\[\]\(\)\~\`\>\#\+\-\=\|\{\}\.\!]"
+    return re.sub(special_chars, r"\\\g<0>", text)
