0e9ffc816d803e1dcbd6d064860ba4ad5e69e2c1
Fstream7 <ivan.tkachuk@fotbo.org>
2024-12-30 20:11:38 +0200

    fix lint

--- app/handlers/privacy.py
+++ app/handlers/privacy.py
@@ -1,6 +1,5 @@
 from aiogram.filters import Command
 from aiogram import Router, types
-from utils.escape_for_markdown import escape_markdown
 
 router = Router()
 
@@ -12,11 +11,11 @@ By sending messages to this bot in private chat, you agree to the following priv
 It can be viewed at any time by using the `/privacy` command in private chat with the bot.
 
 Data Collection
-Fumobot don't collect or save any received data. 
-All received messages in direct chat wih bot are forwarding to bot administrator and can be posted publicly 
-This data is only stored on Telegram servers, bot don't store them. Bot also sends your name and login for feedback. 
-If you do not want your account to be viewed on forwarding to channel, 
-disable link for forwarding in Telegram privacy settings or write in the message that you want to remain anonymous.
+Fumobot don't collect or save any received data.
+All received messages in direct chat wih bot are forwarding to bot administrator and can be posted publicly
+This data is only stored on Telegram servers, bot don't store them. Bot also sends your name and login for feedback.
+If you do not want your account to be viewed on forwarding to channel, disable link for forwarding in
+Telegram privacy settings or write in the message that you want to remain anonymous.
 For /fumofumo command fumobot will get your user_id in Telegram for processing.
 
 
