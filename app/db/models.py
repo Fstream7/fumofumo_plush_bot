d4514e663a2b7b14ecf60134364594ad6a1fb946
Fstream7 <ivan.tkachuk@fotbo.org>
2025-01-04 15:16:38 +0200

    init db tests

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Integer
-from db.base import Base
+from .base import Base
 
 
 class Fumo(Base):
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,11 @@ import pytest
 from aiogram import Dispatcher
 from tests.mocked_bot import MockedBot
 from app.handlers import start
+from app.db.base import Base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def bot() -> MockedBot:
     return MockedBot()
 
@@ -14,3 +16,31 @@ def dp() -> Dispatcher:
     dispatcher = Dispatcher()
     dispatcher.include_router(start.router)
     return dispatcher
+
+
+@pytest.fixture(scope="session")
+def async_engine():
+    """
+    Create async engine for tests
+    aiosqlite cant work well with in-memory db, so create db file
+    """
+    engine = create_async_engine("sqlite+aiosqlite:///tests/testdb.sqlite", echo=False)
+    try:
+        yield engine
+    finally:
+        engine.sync_engine.dispose()
+
+
+@pytest.fixture(scope="function")
+async def async_session(async_engine):
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    sessionmaker = async_sessionmaker(async_engine, expire_on_commit=False)
+    async with sessionmaker() as session:
+        try:
+            yield session
+        finally:
+            await session.rollback()
+            await session.close()
+            async with async_engine.begin() as conn:
+                await conn.run_sync(Base.metadata.drop_all)
