c7a4636e8a1ebabec313b55253c2e6249d45d16f
Fstream7 <ivan.tkachuk@fotbo.org>
2025-03-02 16:48:13 +0200

    rename chat members to group members

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 from . import get_id
 from . import fumofumo
-from . import chat_members
+from . import group_members
 from . import privacy
 from . import start
 from . import private_admin
@@ -16,5 +16,5 @@ def collect_routers() -> list[Router]:
         get_id.router,
         private_admin.router,
         private_users.router,
-        chat_members.router,
+        group_members.router,
     ]
