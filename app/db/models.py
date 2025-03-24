f038fd7bf33820c232029b649b6c4de20a183b41
Fstream7 <vania.tkachuk@gmail.com>
2025-03-24 19:47:46 +0200

    fix lint

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, Float
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from .base import Base
 
