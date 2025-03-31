6453948fbd0cba4a031a5f413e19a3c7a8676573
Fstream7 <vania.tkachuk@gmail.com>
2025-03-31 21:27:10 +0300

    make nix-shell to auto activate venv and install depencies if not installed

--- shell.nix
+++ shell.nix
@@ -4,5 +4,18 @@ mkShell {
   shellHook = ''
     # fixes libstdc++ issues and libgl.so issues
     LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/
+
+    # Python virtual environment setup
+    VENV_DIR=".venv"
+    if [ ! -d "$VENV_DIR" ]; then
+      echo "Creating virtual environment..."
+      python -m venv $VENV_DIR
+      source $VENV_DIR/bin/activate
+      echo "Installing depencies..."
+      pip3 install -r requirements.txt
+    else
+      source $VENV_DIR/bin/activate
+    fi
+    echo "Virtual environment activated."
   '';
 }
